42eac40d260d0b2d7d62e4b1368451c01e928cad
Fraz-Xprolabs <muhammad.fraz@xprolabs.com>
2025-07-24 18:58:24 +0500

    Initialize blog controller with index, show, new, and create, update, destroy actions

---
+++ app/controllers/blogs_controller.rb
@@ -0,0 +1,51 @@
+class BlogsController < ApplicationController
+  before_action :set_blog, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @blogs = Blog.all
+    puts "DEBUG: Blogs count => #{@blogs.count}"
+  end
+
+
+  def show
+  end
+
+  def new
+    @blog = Blog.new
+  end
+
+  def create
+    @blog = Blog.new(blog_params)
+    if @blog.save
+      redirect_to @blog
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @blog.update(blog_params)
+      redirect_to @blog
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @blog.destroy
+    redirect_to blogs_path
+  end
+
+  private
+
+  def set_blog
+    @blog = Blog.find(params[:id])
+  end
+
+  def blog_params
+    params.require(:blog).permit(:title, :content)
+  end
+end
