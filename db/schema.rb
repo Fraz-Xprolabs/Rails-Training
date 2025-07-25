8864c3cf61d8e3129232db55847dc8ea9286fc9d
Fraz-Xprolabs <muhammad.fraz@xprolabs.com>
2025-07-25 19:54:17 +0500

    Incorporated Reviewed Changes

--- db/schema.rb
+++ db/schema.rb
@@ -21,10 +21,4 @@ ActiveRecord::Schema[8.0].define(version: 2025_07_24_122031) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "posts", force: :cascade do |t|
-    t.string "title"
-    t.text "content"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
 end
